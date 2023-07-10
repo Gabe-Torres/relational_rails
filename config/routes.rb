bff06284360645b7992eec78b5c867c80b48f518
Gabriel Torres <thomastgtorres21@gmail.com>
2023-07-09 23:17:18 -0500

    add: routes for paths

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/", to: "welcome#index"
+  get "/benders", to: "benders#index"
+  get "/benders/new", to: "benders#new"
+  get "/benders/:id", to: "benders#show"
+  get "/nations", to: "nations#index"
+  get "/nations/:id", to: "nations#show"
+  get "/nations/:nation_id/benders", to: "nation_benders#index"
 end
